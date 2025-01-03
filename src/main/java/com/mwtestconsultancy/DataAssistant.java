b8a564a413ada853e08c733d325015351b11e6d7
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2025-01-03 18:11:43 +0000

    Added in agent example code

--- src/main/java/com/mwtestconsultancy/DataAssistant.java
+++ src/main/java/com/mwtestconsultancy/DataAssistant.java
@@ -4,25 +4,34 @@ import dev.langchain4j.model.openai.OpenAiChatModel;
 import dev.langchain4j.model.openai.OpenAiChatModelName;
 import dev.langchain4j.service.AiServices;
 
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class DataAssistant {
 
     static interface DataAssistantService {
         String sendPrompt(String userPrompt);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         OpenAiChatModel model = OpenAiChatModel
                 .builder()
                 .apiKey(System.getenv("OPENAI_KEY"))
-                .modelName(OpenAiChatModelName.GPT_3_5_TURBO)
+                .modelName(OpenAiChatModelName.GPT_4_O_MINI)
                 .build();
 
         DataAssistantService dataAssistantChat = AiServices.builder(DataAssistantService.class)
                 .chatLanguageModel(model)
+                .tools(new DataAssistantTools())
                 .build();
 
-        String response = dataAssistantChat.sendPrompt("Hi, can you introduce yourself?");
-        System.out.printf(response);
+        while(true){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("What do you need?");
+            String query = scanner.nextLine();
+            String response = dataAssistantChat.sendPrompt(query);
+            System.out.println(response);
+        }
     }
 
 }
